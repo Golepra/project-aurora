c9b9e5c8dba65306b73eb4488e6a0931d1fe0ae9
Golepra <tamangpratik2063@gmail.com>
2026-08-15 18:44:23 +0545

    Enhance chapter click event with story display

--- js/chapter.js
+++ js/chapter.js
@@ -1,7 +1,22 @@
 const chapter = document.getElementById("chapterOne");
+const story = document.getElementById("storyScene");
 
-chapter.addEventListener("click", () => {
+chapter.addEventListener("click",()=>{
 
     chapter.classList.add("moveAway");
+
+    setTimeout(()=>{
+
+        chapter.style.display="none";
+
+        story.classList.add("show");
+
+        story.scrollIntoView({
+
+            behavior:"smooth"
+
+        });
+
+    },1200);
 
 });
